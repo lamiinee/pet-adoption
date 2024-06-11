e1099aa98c99da25c1a22cd02eb9c2ee040d701a
lamiinee <mouhamadouza@gmail.com>
2024-06-11 12:11:26 -0400

    Finished pet filter area

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ async function petArea() {
   const petData = await petPromise.json()
   petData.forEach(pet => {
     const clone = template.content.cloneNode(true)
+
+    clone.querySelector(".pet-card").dataset.species = pet.species
     clone.querySelector("h3").textContent = pet.name
     clone.querySelector(".pet-description").textContent = pet.description
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
@@ -49,4 +51,15 @@ function handleButtonClick(e) {
   allButtons.forEach(el => el.classList.remove("active"))
 
   e.target.classList.add("active")
+
+  const currentFilter = e.target.dataset.filter
+
+  document.querySelectorAll(".pet-card").forEach(el => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid"
+    }
+    else {
+      el.style.display = "none"
+    }
+  })
 }
